60033655a12ba7c472454198e14fda6b9ddf8c05
Atakan Yontar <atakanyontar@gmail.com>
2022-05-13 14:05:22 +0300

    refactor: changed versions of packages
    To stabilize the Onion Architecture environment, I upgraded .net from 3.1 to 6.0 and depending NuGet packages accordingly

--- OnionArchitecture/WebApi/Startup.cs
+++ OnionArchitecture/WebApi/Startup.cs
@@ -1,4 +1,4 @@
-
+using Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -27,7 +27,9 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplication();
             services.AddControllers();
+            
             #region Swagger
             services.AddSwaggerGen(c =>
             {
