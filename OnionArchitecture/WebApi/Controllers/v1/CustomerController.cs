4769d0edbcdc02e51730adf2b420ded7ebb3f976
Aycan <aycnkya12@gmail.com>
2022-05-19 11:45:18 +0300

    CRUD operations for customer

---
+++ OnionArchitecture/WebApi/Controllers/v1/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using Application.Features.CustomerFeatures.Commands;
+using Application.Features.ProductFeatures.Commands;
+using Application.Features.ProductFeatures.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers;
+
+[ApiVersion("1.0")]
+public class CustomerController : BaseApiController
+{
+    /// <summary>
+    /// Creates a New Product.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateCustomerCommand command)
+    {
+        return Ok(await Mediator.Send(command));
+    }
+    /// <summary>
+    /// Gets all Products.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await Mediator.Send(new GetAllCustomerQuery()));
+    }
+    /// <summary>
+    /// Gets Product Entity by Id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok(await Mediator.Send(new GetCustomerQuery { Id = id }));
+    }
+    /// <summary>
+    /// Deletes Product Entity based on Id.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return Ok(await Mediator.Send(new DeleteCustomerCommand { Id = id }));
+    }
+    /// <summary>
+    /// Updates the Product Entity based on Id.   
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    [HttpPut("[action]")]
+    public async Task<IActionResult> Update(int id, UpdateCustomerCommand command)
+    {
+        if (id != command.Id)
+        {
+            return BadRequest();
+        }
+        return Ok(await Mediator.Send(command));
+    }
+
+    
+}
--- OnionArchitecture/WebApi/Startup.cs
+++ OnionArchitecture/WebApi/Startup.cs
@@ -30,6 +30,18 @@ namespace WebApi
             services.AddApplication();
             services.AddControllers();
             services.AddPersistence(Configuration);
+            #region API Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+            #endregion
 
             #region Swagger
             services.AddSwaggerGen(c =>
